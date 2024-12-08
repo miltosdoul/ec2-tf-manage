d0986c1764d8b36e03e6fde06a6b0089cab46993
miltosdoul <miltosdoul@yahoo.com>
2024-12-08 18:39:19 +0200

    Try removing port range from egress

--- main.tf
+++ main.tf
@@ -46,8 +46,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
   security_group_id = aws_security_group.allow_all_egress.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
-  to_port     = 65535
   ip_protocol = "-1"
 }
 
