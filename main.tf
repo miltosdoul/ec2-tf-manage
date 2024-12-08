ec180d3ee2fdec4e35a29a32020a1d999f8c53ea
miltosdoul <miltosdoul@yahoo.com>
2024-12-08 15:42:58 +0200

    Format

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
     name   = "architecture"
     values = ["x86"]
   }
-  
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -27,10 +27,10 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.allow_ssh.id
 
-  cidr_ipv4 = "0.0.0.0/0"
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  to_port     = 22
+  ip_protocol = "tcp"
 }
 
 resource "aws_security_group" "allow_all_egress" {
@@ -52,11 +52,11 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
 }
 
 resource "aws_instance" "ec2-instance" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
   associate_public_ip_address = true
-  security_groups = [ aws_security_group.allow_ssh.id, aws_security_group.allow_all_egress.id ]
+  security_groups             = [aws_security_group.allow_ssh.id, aws_security_group.allow_all_egress.id]
 
   tags = {
     Name = "main_instance"
