d709a234884e6ce5a2c98db8db88111b52a04fc5
miltosdoul <miltosdoul@yahoo.com>
2024-12-08 18:36:47 +0200

    Update egress port range

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 0
-  to_port     = 0
+  to_port     = 65535
   ip_protocol = "-1"
 }
 
