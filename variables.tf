67fca1aed2f6260ddc73a145332ce0195520a7df
miltosdoul <miltosdoul@yahoo.com>
2024-12-08 15:44:24 +0200

    Remove unused variables

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "aws_account_id" {
-  type        = string
-  description = "Provided via workflow's environment"
-}
-
-variable "aws_terraform_role" {
-  type        = string
-  description = "Provided via workflow's environment"
-}
-
 variable "ami_name_regex" {
   description = "The name filter for AMI"
   type        = string
