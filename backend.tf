7ca7fe4c716bb5822acc6dbd6d3583fb179a346c
miltosdoul <miltosdoul@yahoo.com>
2024-12-08 15:27:22 +0200

    Try empty 'role_arn'

--- backend.tf
+++ backend.tf
@@ -4,6 +4,8 @@ terraform {
     key    = "terraform.tfstate"
     # assume_role uses AWS_ROLE_ARN env var
     # https://developer.hashicorp.com/terraform/language/backend/s3#assume-role-with-web-identity-configuration
-    assume_role_with_web_identity = {}
+    assume_role_with_web_identity = {
+      role_arn = ""
+    }
   }
 }
